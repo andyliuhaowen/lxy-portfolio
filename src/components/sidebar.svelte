f0834a5fbdd4aff9a31c60b93bc2200279e6cafd
Haowen Liu <lhw@lunacd.com>
2022-06-13 21:46:45 -0700

    Add sidebar

---
+++ src/components/sidebar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Logo from '../images/logo.svg';
+</script>
+
+<div class="px-24 text-lg items-center bg-blue-200">
+  <div class="w-36 h-screen flex flex-col justify-between py-24">
+    <img src={Logo} alt="Shirley Lyu Logo" class="w-full" />
+    <div class="flex flex-col space-y-2">
+      <a href="/about" class="primary">About</a>
+      <a href="/portfolio" class="primary">Portfolio</a>
+
+      <a href="/overlap" class="secondary">Overlap</a>
+      <a href="/lyu" class="secondary">Lyu</a>
+      <a href="/soul" class="secondary">Soul</a>
+      <a href="/sunrise" class="secondary">Sunrise Speaker</a>
+      <a href="/mode" class="secondary">Mode Bag</a>
+      <a href="/More" class="secondary">More</a>
+
+      <a href="/mini" class="primary">Mini Works</a>
+      <a href="/resume" class="primary pb-8">Resume</a>
+    </div>
+    <div class="flex flex-row justify-between">
+      <i class="fa-brands fa-instagram" />
+      <i class="fa-regular fa-envelope" />
+      <i class="fa-brands fa-linkedin-in" />
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .primary {
+    @apply font-semibold;
+  }
+
+  .secondary {
+    @apply font-extralight;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import '../app.css';
+  import '../app.css';
 </script>
 
-<slot />
+<div class="hidden md:block">
+  <slot />
+</div>
+<div class="block md:hidden m-12 font-semibold">
+  Your screen is too small. Use a larger device or enlarge your browser window.
+</div>
