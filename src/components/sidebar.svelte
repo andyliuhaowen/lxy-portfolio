72e0eb2fcf3426ded6e2cedd2f2101146433c123
Haowen Liu <lhw@lunacd.com>
2022-06-13 23:14:31 -0700

    Add collapse and expand

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import Logo from '../images/logo.svg';
+  import { spring } from 'svelte/motion';
+
+  var progress = spring(1, {stiffness: 0.1, damping: 0.8});
+  var open = true;
 </script>
 
-<div class="px-24 text-lg items-center">
-  <div class="w-36 h-screen flex flex-col justify-between py-24">
+<div class="relative w-84 h-screen text-lg shadow-lg" style={`width: ${$progress * 21}rem`}>
+  <div class="absolute top-0 right-24 w-36 h-screen flex flex-col justify-between py-24">
     <img src={Logo} alt="Shirley Lyu Logo" class="w-full" />
     <div class="flex flex-col space-y-2">
       <a href="/about" class="primary">About</a>
@@ -20,9 +24,30 @@
       <a href="/resume" class="primary pb-8">Resume</a>
     </div>
     <div class="flex flex-row justify-between">
-      <i class="fa-brands fa-instagram" />
-      <i class="fa-regular fa-envelope" />
-      <i class="fa-brands fa-linkedin-in" />
+      <a href="https://www.instagram.com/lyu.shirley/">
+        <i class="fa-brands fa-instagram" />
+      </a>
+      <a href="mailto:shirley.lyu.xiaoya@gmail.com">
+        <i class="fa-regular fa-envelope" />
+      </a>
+      <a href="https://www.linkedin.com/in/xiaoya-lyu-11b68419b/">
+        <i class="fa-brands fa-linkedin-in" />
+      </a>
+    </div>
+  </div>
+  <div class="absolute w-20 h-24 top-20 -right-20 overflow-hidden">
+    <div
+      class="absolute rounded-full bg-white w-20 h-20 top-0 -left-10 shadow-lg flex items-center cursor-pointer"
+      on:click={() => {
+        if (open) {
+          $progress = 0;
+        } else {
+          $progress = 1;
+        }
+        open = !open;
+      }}
+    >
+      <i class="relative fa-solid fa-angle-left fa-xl left-12" style={`transform: rotateY(${(1 - $progress) * 180}deg)`} />
     </div>
   </div>
 </div>
